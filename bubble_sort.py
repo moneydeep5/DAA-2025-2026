abefe93ad1255629482710ec15a7f616adf2dce6
MANI DEEP <gmanideep512@gmail.com>
2026-07-20 22:52:01 +0530

    initial commit

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+a = input("Enter the numbers: ").split()
+for i in range(len(a)):
+    a[i] = int(a[i])
+n = len(a)
+
+for i in range(n):
+    for j in range(n - 1):
+        if a[j] > a[j + 1]:
+            temp = a[j]
+            a[j] = a[j + 1]
+            a[j + 1] = temp
+print("Sorted numbers are:")
+print(a)
